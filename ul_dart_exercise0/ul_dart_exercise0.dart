9932ae952c451b7397c9ddaff5d7a80f623dee2a
Maxime Fortier <fortier.maxime@gmail.com>
2013-02-10 09:41:41 -0500

    Q3 and Q4 added

--- ul_dart_exercise0/ul_dart_exercise0.dart
+++ ul_dart_exercise0/ul_dart_exercise0.dart
@@ -37,8 +37,56 @@ main() {
   
   print('Aire du cercle avec un rayon de $rayon:  $aire');
   
+  /*
+  * 
+  *      Q 0.3
+  *
+  *    A rare country jewel: Ready to build country lot 
+  *    (220 X 260 feet) for your dream home in a clean, 
+  *    quiet and peaceful rural setting.
+  *    What is the size in acres of this lot?
+  *
+  */  
   
+  print('----------------- Q3 ---------------');
   
-  
+  // 43,560 sq feet in an acre
+  var longeur = 260;
+  var largeur = 220;
  
+  var airelot = longeur * largeur;
+  
+  print('$airelot pied carré');
+  airelot = airelot / 43560;
+  print('$airelot acres');
+
+  
+  /*
+  * 
+  *      Q 0.4
+  *
+  *    In Berlin in 2009, Usain Bolt 
+  *    broke his Olympic record on 
+  *    100 meters to win at 9.58 seconds. 
+  *    How fast he was running?
+  *
+  */  
+  
+  print('----------------- Q4 ---------------');
+  
+  print('Answer is he was running real fast!');
+  
+  //    100       = 10.4381.... =    375477.16      / 1000 = 37.54....
+  //    -----       ----            ------
+  //    9.58 sec    1 sec           60x60 = 3600 
+  
+  var resultat = (100 * 1) / 9.58;
+  resultat = resultat * (60 * 60) / 1000;
+  
+  print('No joke... $resultat km/h');
+  
+  
+  
+  
+  
 }
